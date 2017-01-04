7198887007f3fee26ac10bc9f03f69209a608c32
forkwd <wdandev@gmail.com>
2017-01-04 11:00:25 +0800

    fix report layout

--- app/src/main/java/com/uteamtec/heartcool/service/utils/DateFormats.java
+++ app/src/main/java/com/uteamtec/heartcool/service/utils/DateFormats.java
@@ -8,11 +8,20 @@ import java.util.Locale;
  */
 public final class DateFormats {
 
-    public static final SimpleDateFormat YYYY = new SimpleDateFormat("yyyy", Locale.getDefault());
-    public static final SimpleDateFormat YYYY_MM = new SimpleDateFormat("yyyy-MM", Locale.getDefault());
-    public static final SimpleDateFormat YYYY_MM_DD = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-    public static final SimpleDateFormat YYYY_MM_DD_HH_MM_SS = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss", Locale.getDefault());
+    public static final SimpleDateFormat YYYY = new SimpleDateFormat(
+            "yyyy", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM = new SimpleDateFormat(
+            "yyyy-MM", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM_DD = new SimpleDateFormat(
+            "yyyy-MM-dd", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM_DD_HH_MM_SS = new SimpleDateFormat(
+            "yyyy-MM-dd HH-mm-ss", Locale.getDefault());
 
-    public static final SimpleDateFormat YYYY_MM_CN = new SimpleDateFormat("yyyy年MM月", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM_CN = new SimpleDateFormat(
+            "yyyy年MM月", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM_DD_HH_MM_CN = new SimpleDateFormat(
+            "yyyy年MM月dd日 HH时mm分", Locale.getDefault());
+    public static final SimpleDateFormat YYYY_MM_DD_HH_MM_SS_CN = new SimpleDateFormat(
+            "yyyy年MM月dd日 HH时mm分ss秒", Locale.getDefault());
 
 }
