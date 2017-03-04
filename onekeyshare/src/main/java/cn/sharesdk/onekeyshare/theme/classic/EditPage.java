4b7b94e4b9e8646064eabbd401b58859f4702431
forkwd <wdandev@gmail.com>
2017-03-04 11:04:48 +0800

    feat: support release

--- onekeyshare/src/main/java/cn/sharesdk/onekeyshare/theme/classic/EditPage.java
+++ onekeyshare/src/main/java/cn/sharesdk/onekeyshare/theme/classic/EditPage.java
@@ -59,6 +59,7 @@ import cn.sharesdk.framework.TitleLayout;
 import com.mob.tools.utils.UIHandler;
 import cn.sharesdk.onekeyshare.EditPageFakeActivity;
 import cn.sharesdk.onekeyshare.PicViewer;
+import cn.sharesdk.onekeyshare.R;
 import cn.sharesdk.onekeyshare.ShareCore;
 
 /**
@@ -183,7 +184,7 @@ public class EditPage extends EditPageFakeActivity implements OnClickListener, T
 	// title bar
 	private TitleLayout getPageTitle() {
 		llTitle = new TitleLayout(getContext());
-		llTitle.setId(1);
+		llTitle.setId(R.id.titleLayout);
 //		int resId = getBitmapRes(activity, "title_back");
 //		if (resId > 0) {
 //			llTitle.setBackgroundResource(resId);
@@ -212,7 +213,7 @@ public class EditPage extends EditPageFakeActivity implements OnClickListener, T
 	// page body
 	private LinearLayout getPageBody() {
 		llBody = new LinearLayout(getContext());
-		llBody.setId(2);
+		llBody.setId(R.id.imagesLinearLayout);
 		int resId = getBitmapRes(activity, "edittext_back");
 		if (resId > 0) {
 			llBody.setBackgroundResource(resId);
@@ -274,7 +275,7 @@ public class EditPage extends EditPageFakeActivity implements OnClickListener, T
 	// shared image container
 	private RelativeLayout getThumbView() {
 		rlThumb = new RelativeLayout(getContext());
-		rlThumb.setId(1);
+		rlThumb.setId(R.id.mainRelLayout);
 		int dp_82 = dipToPx(getContext(), 82);
 		int dp_98 = dipToPx(getContext(), 98);
 		LayoutParams lpThumb
